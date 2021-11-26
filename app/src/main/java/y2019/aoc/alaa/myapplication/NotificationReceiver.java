90fe4d962813761ccea25cfeed45611fc9ab4eb4
AndreMakhoul <makhoul.andre1@gmail.com>
2021-11-26 08:43:34 +0200

    added

---
+++ app/src/main/java/y2019/aoc/alaa/myapplication/NotificationReceiver.java
@@ -0,0 +1,13 @@
+package y2019.aoc.alaa.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
