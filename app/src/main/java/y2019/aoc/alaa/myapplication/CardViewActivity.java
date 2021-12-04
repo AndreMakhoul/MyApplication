5a4b57e7315d003b2eeceb45a68cc16580a54d10
AndreMakhoul <makhoul.andre1@gmail.com>
2021-12-04 20:24:32 +0200

    added

--- app/src/main/java/y2019/aoc/alaa/myapplication/CardViewActivity.java
+++ app/src/main/java/y2019/aoc/alaa/myapplication/CardViewActivity.java
@@ -2,7 +2,9 @@ package y2019.aoc.alaa.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CardViewActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class CardViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card_view);
+    }
+
+
+    public void toArrayList(View view) {
+        Intent intent = new Intent(this, ArrayListActivity.class);
+        startActivity(intent);
+
     }
 }
